1f154cfb921038f817d19588b62370e6354673d5
ali-fr-2 <alifarjamy2@gmail.com>
2026-07-10 19:43:02 +0330

    159-startmvc-part2

--- index.php
+++ index.php
@@ -1,5 +1,45 @@
-<?php 
+<?php
 
-echo "MVC";
+// class Person
+// {
+//     public $name;
+//     public $sport;
+
+//     public function play()
+//     {
+//         echo $this->name . "   " . "plays" . "  " . $this->sport;
+//     }
+// }
+
+// $man = new Person();
+// $man->name = "ali";
+// $man->sport = "volleyball";
+// $man->play();
+// echo "<br>";
+// $man = new Person();
+// $man->name = "ali";
+// $man->sport = "volleyball";
+// $man->play();
+
+class pet{
+
+public $voice;
+
+public function myVoice(){
+    echo $this->voice;
+}
+
+}
+
+$cat=new pet();
+
+$cat->voice="<script> alert('meow')</script>";
+$cat->myVoice();
+
+
+// we could create two objdect with same name but we couldnt create two property with same name ;
 
 ?>
+
+
+
