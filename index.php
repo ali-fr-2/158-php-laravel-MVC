594316f0bcb3b58df5cb2cef3ec5489404adc743
ali-fr-2 <alifarjamy2@gmail.com>
2026-07-13 23:10:29 +0330

    163-__construct

--- index.php
+++ index.php
@@ -1,30 +1,56 @@
 <?php
 
-// class A{
-//     public $color="blue";
-// }
-class A{
-    protected $color="blue";
-}
-// class A{
-//     private $color="blue";
+// class Person{
+//     function __construct()
+//     {
+//         echo "hello world";
+//     }
+
+//     function show(){
+//         return "my name is ali";
+//     }
 // }
 
-class B extends A{
-    public $age=22;
-    function ShowColor(){
-       return $this->color;
+// $a=new Person();
+// echo "<br/>";
+// echo $a->show();
+//------------------------------
+
+// class Time{
+//     private $hour;
+//     private $minute;
+//     private $second;
+//     function __construct($hour=0,$minute=0,$second=0)
+//     {
+//         $this->hour=$hour;
+//         $this->minute=$minute;
+//         $this->second=$second;
+//         echo $this->hour." : ".$this->minute." : ".$this->second;
+//     }
+// }
+
+// // $time=new Time();
+// $time=new Time(16,25,50);
+
+class Time{
+    private $hour;
+    private $minute;
+    private $second;
+    function __construct($hour=0,$minute=0,$second=0)
+    {
+        $this->hour=$hour;
+        $this->minute=$minute;
+        $this->second=$second;
+    }
+    function gettime(){
+        return $this->hour." : ".$this->minute." : ".$this->second;
     }
 }
 
-class C extends B{
-    function getone(){
-        return $this->color." ".$this->age;
-    }
-}
+// $time=new Time();
+$time=new Time(16,25,50);
+echo $time->gettime();
 
-$c=new C();
-echo $c->getone();
 
 ?>
 
