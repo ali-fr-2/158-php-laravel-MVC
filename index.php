2e857aa4e8a67a8cede37f43c28ec6f03b6f40fc
ali-fr-2 <alifarjamy2@gmail.com>
2026-07-14 18:52:16 +0330

    164-__destruct

--- index.php
+++ index.php
@@ -1,58 +1,16 @@
 <?php
 
-// class Person{
-//     function __construct()
-//     {
-//         echo "hello world";
-//     }
-
-//     function show(){
-//         return "my name is ali";
-//     }
-// }
-
-// $a=new Person();
-// echo "<br/>";
-// echo $a->show();
-//------------------------------
-
-// class Time{
-//     private $hour;
-//     private $minute;
-//     private $second;
-//     function __construct($hour=0,$minute=0,$second=0)
-//     {
-//         $this->hour=$hour;
-//         $this->minute=$minute;
-//         $this->second=$second;
-//         echo $this->hour." : ".$this->minute." : ".$this->second;
-//     }
-// }
-
-// // $time=new Time();
-// $time=new Time(16,25,50);
-
-class Time{
-    private $hour;
-    private $minute;
-    private $second;
-    function __construct($hour=0,$minute=0,$second=0)
+class Person
+{
+    function showinfo()
     {
-        $this->hour=$hour;
-        $this->minute=$minute;
-        $this->second=$second;
+        echo "hello world";
     }
-    function gettime(){
-        return $this->hour." : ".$this->minute." : ".$this->second;
+    function __destruct()
+    {
+        echo "this is destruct ";
     }
 }
-
-// $time=new Time();
-$time=new Time(16,25,50);
-echo $time->gettime();
-
-
-?>
-
-
-
+$ali=new Person();
+// die();
+unset($ali);
